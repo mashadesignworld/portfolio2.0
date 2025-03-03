d66e328762546c3a61df5c13eadad4187396c488
kevin macharia <njugunak.kevin@gmail.com>
2025-03-03 18:22:22 +0300

    final commit

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -6,18 +6,51 @@ import  { motion } from "motion/react";
 
 const Services = () => {
   return (
-    <motion.div id='services' className='w-full px-[12%] py-10 scroll-mt-2'>
-        <h4 className='text-center mb-2 text-lg font-Ovo'>
+    <motion.div 
+    initial={{ opacity: 0}}
+    whileInView={{ opacity: 1}}
+    transition={{ duration: 1 }}
+    
+    
+    
+    id='services' className='w-full px-[12%] py-10 scroll-mt-2'>
+        <motion.h4 
+        
+        initial={{y: -20, opacity: 0}}
+        whileInView={{ y:0, opacity: 1}}
+        transition={{ delay: 0.3, duration: 0.5}}
+        
+        className='text-center mb-2 text-lg font-Ovo'>
             What i offer
-        </h4>
-        <h2 className='text-center text-5xl font-Ovo'>
+        </motion.h4>
+        <motion.h2
+         initial={{y: -20, opacity: 0}}
+         whileInView={{ y:0, opacity: 1}}
+         transition={{ delay: 0.5, duration: 0.5}}
+        
+        className='text-center text-5xl font-Ovo'>
           Services
-        </h2>
-        <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>I provide full-stack web development services, creating modern, scalable,
-             and user-friendly applications tailored to your needs, with a focus on high-performance solutions.</p>
-        <div className='grid grid-cols-auto gap-6 my-10'>
+        </motion.h2>
+        <motion.p 
+        
+        initial={{y: -20, opacity: 0}}
+        whileInView={{ y:0, opacity: 1}}
+        transition={{ delay: 0.7, duration: 0.5}}
+        
+        className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>I provide full-stack web development services, creating modern, scalable,
+             and user-friendly applications tailored to your needs, with a focus on high-performance solutions.</motion.p>
+        <motion.div 
+        
+        initial={{y: -20, opacity: 0}}
+        whileInView={{ y:0, opacity: 1}}
+        transition={{ delay: 0.9, duration: 0.6}}
+        
+        
+        className='grid grid-cols-auto gap-6 my-10'>
                 {serviceData.map(({icon, title, description, link}, index)=>(
-                    <div key={index} className='border border-gray-400 rounded-lg px-8 px-12
+                    <motion.div 
+                    whileHover={{scale: 1.05}}
+                    key={index} className='border border-gray-400 rounded-lg px-8 px-12
                     hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500'>
                         <Image src={icon} alt="services" className='w-10 my-5' />
                         <h3 className='text-lg my-4 text-gray-700 dark:text-white'>{title}</h3>
@@ -25,9 +58,9 @@ const Services = () => {
                         {description}
                     </p>
                     
-                    </div>
+                    </motion.div>
                 ))}
-        </div>
+        </motion.div>
     </motion.div>
   )
 }
--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -2,23 +2,45 @@ import { workData } from '@/assets/assets'
 import React from 'react'
 import Image from 'next/image'
 import { assets } from '@/assets/assets'
+import  { motion } from "motion/react";
 
 const Work = ({isDarkMode, setIsDarkMode}) => {
   return (
     <div id='work' className='w-full px-[12%] py-10 scroll-mt-2'>
       
       
-<h4 className='text-center mb-2 text-lg font-Ovo'>
+<motion.h4 
+  initial={{ opacity: 0, y:-20 }}
+  whileInView={{ opacity: 1, y:0 }}
+  transition={{duration: 0.5, delay: 0.3}}
+
+className='text-center mb-2 text-lg font-Ovo'>
            My Work
-        </h4>
-        <h2 className='text-center text-5xl font-Ovo'>
+        </motion.h4>
+        <motion.h2 
+        
+        initial={{ opacity: 0, y:-20 }}
+        whileInView={{ opacity: 1, y:0 }}
+        transition={{duration: 0.5, delay: 0.5}}
+        
+        className='text-center text-5xl font-Ovo'>
           Latest Projects
-        </h2>
-        <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>I provide full-stack web development services, creating modern, scalable,
-             and user-friendly applications tailored to your needs, with a focus on high-performance solutions.</p>        
+        </motion.h2>
+        <motion.p 
+        
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{duration: 0.6, delay:0.8}}
+        
+        className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>I provide full-stack web development services, creating modern, scalable,
+             and user-friendly applications tailored to your needs, with a focus on high-performance solutions.</motion.p>        
         <div className='grid grid-cols-auto my-10 gap-5'>
           {workData.map((project, index)=>(
-            <div key={index} 
+            <motion.div 
+            
+            whileHover={{ scale: 1.05}}
+            
+            key={index} 
             className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group'
             style={{backgroundImage: `url(${project.bgImage})`}}>
               <div className='bg-white w-10/12 rounded-md 
@@ -35,7 +57,7 @@ const Work = ({isDarkMode, setIsDarkMode}) => {
                    <Image src={assets.send_icon} alt="send" className='w-5'/>
                 </div>
               </div>
-            </div>
+            </motion.div>
           ))}
         </div>
     
